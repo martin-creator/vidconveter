1119048ceeac8c733a0dee95b7002e2ea03d26b7
martin-creator <martinlubowa@outlook.com>
2023-03-28 04:29:37 +0300

    Added config for gateway

---
+++ python/src/gateway/server.py
@@ -0,0 +1,20 @@
+import os, gridfs, pika, json
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
+
+mongo = PyMongo(server)
+
+fs = gridfs.GridFS(mongo.db) # gridfs helps to store large files
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
